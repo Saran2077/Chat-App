bed0bfb770f0e89fe1d4f6d6c3dea92633a6bf78
Saran2077 <saranjamespond123@gmail.com>
2024-03-18 00:03:18 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Register from "./Register";
+
+const App = () => {
+  return(
+    <div className="bg-red-500">
+      Saran
+      <Register />
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/Register.jsx
@@ -0,0 +1,7 @@
+const Register = () => {
+    return(
+        <div className="bg-blue-50 h-screen">Register</div>
+    )
+}
+
+export default Register;
